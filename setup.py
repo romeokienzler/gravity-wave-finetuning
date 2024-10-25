b1c3d07b9f1b02228ae66a564fccb1adfd1be295
Romeo Kienzler <romeo.kienzler1@ibm.com>
2024-10-25 13:16:17 +0200

    fix install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prithviwxc-gravitywave",
+    version="0.1.0",
+    description="Gravity Wave Parameterization",
+    packages=find_packages(where="."),
+    include_package_data=True,
+)
